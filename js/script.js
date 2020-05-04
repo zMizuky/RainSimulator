2b560cb018c5a75beae5fb248fe5100b76600390
Mizuky <48653877+zMizuky@users.noreply.github.com>
2020-05-04 14:38:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+var img = [
+    "Anastasia Shuraeva",
+    "Lucas Craig",
+    "Pixabay",
+    "Stefan Stefancik",
+    "Stein Egil Liland"
+];
+
 var rain = document.getElementById("rain");
 var rain1 = document.getElementById("rain1");
 var thunder1 = document.getElementById("thunder1");
@@ -47,3 +55,20 @@ function aleatoryT(){
         thunder1.play();
     }
 }
+
+
+
+function aleatoryImg(){
+    var x = Math.floor((Math.random() * 5));
+    var alImg = img[x]
+    setImage(alImg)
+}
+
+var txtN = document.getElementById("txtN")
+function setImage(img){
+    document.getElementById("html").style.backgroundImage = "url('../imgs/" + img + ".jpeg')"
+    txtN.innerHTML = "Image by: " + img;
+}
+
+aleatoryImg()
+
